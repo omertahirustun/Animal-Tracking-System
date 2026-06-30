3a1010f64e84716a5aa82bfeebaa51e024d1813d
IronSid3e <omertahirustun05@hotmail.com>
2026-06-30 19:48:33 +0300

    fix: react native ve native wind sürüm uyuşmazlıkları giderildi

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import "./global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
-  return <Text>Hayvan takip sistemi</Text>;
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-300">
+      <Text className="text-3xl font-bold text-blue-900">
+        Hayvan Takip Sistemi
+      </Text>
+    </View>
+  );
 }
