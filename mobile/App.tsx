5df61eb87472b2de34c9486b4599aaa17e6f61c9
IronSid3e <omertahirustun05@hotmail.com>
2026-06-30 18:34:23 +0300

    feat: React native ve native wind kuruldu

---
+++ mobile/App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+export default function App() {
+  return <Text>Hayvan takip sistemi</Text>;
+}
